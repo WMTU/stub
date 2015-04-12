79fe21d7d93628866dbdd1cb6dd90056087e3492
Neil Betham <nbetham@me.com>
2015-04-12 16:48:40 -0400

    More user auth boiler plate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :authenticate
+
+  def current_user
+    @current_user ||= User.find_by_id session[:user_id]
+  end
+  helper_method :current_user
+
+  # Check if session is authed
+  def authenticate
+    redirect_to new_sessions_url, danger: 'Please Login' unless current_user
+  end
 end
