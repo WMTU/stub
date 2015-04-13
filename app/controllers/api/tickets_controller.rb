305e7f52098de0e9caeda41e20010cb9faf27fb8
Neil Betham <nbetham@me.com>
2015-04-13 11:54:24 -0400

    create tickets when api is called

--- app/controllers/api/tickets_controller.rb
+++ app/controllers/api/tickets_controller.rb
@@ -1,15 +1,16 @@
 class Api::TicketsController < ApiController
-  include RabbitMq
-
   def band
+    Ticket.create(type: 'band')
     render inline: File.open('./zpls/band-night.zpl').read
   end
 
   def dj
+    Ticket.create(type: 'dj')
     render inline: File.open('./zpls/dj-night.zpl').read
   end
 
   def both
+    Ticket.create(type: 'both')
     render inline: File.open('./zpls/both-night.zpl').read
   end
 end
