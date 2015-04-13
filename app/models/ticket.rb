533254242b9ce73a9adfb87267dcafb105fac098
Neil Betham <nbetham@me.com>
2015-04-13 12:03:17 -0400

    fix ticket model

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,11 +1,12 @@
 class Ticket < ActiveRecord::Base
   before_create :generate_hash
+  self.inheritance_column = :na
 
   private
 
   def generate_hash
     begin
-      self.hash = SecureRandom.hex
+      self.uuid = SecureRandom.hex
     end while self.class.exists?(uuid: uuid)
   end
 end
