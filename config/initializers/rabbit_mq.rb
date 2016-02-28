602184b30fdc5a7718ef7cc8f08bea294b9f6046
Neil Betham <nbetham@me.com>
2016-02-28 00:37:30 -0500

    Switch to slim templates, start using guard for auto reloading during dev

--- config/initializers/rabbit_mq.rb
+++ config/initializers/rabbit_mq.rb
@@ -2,11 +2,21 @@ require 'bunny'
 
 server = CONFIG[:rabbit_mq]
 
+server_config = {
+  host: server[:host],
+  port: server[:port],
+  user: server[:user],
+  pass: server[:pass],
+  vhost: server[:vhost],
+  read_timeout: 30,
+  heartbeat: 2
+}
+
 if defined?(PhusionPassenger) # otherwise it breaks rake commands if you put this in an initializer
   PhusionPassenger.on_event(:starting_worker_process) do |forked|
     if forked
       # Connect to rabbit mq server
-      RABBIT_MQ = Bunny.new "amqp://#{server[:user]}:#{server[:pass]}@#{server[:host]}:#{server[:port]}/#{server[:vhost]}", read_timeout: 30, heartbeat: 2
+      RABBIT_MQ = Bunny.new server_config
       RABBIT_MQ.start
 
       # Setup exchange and channel
@@ -17,13 +27,13 @@ if defined?(PhusionPassenger) # otherwise it breaks rake commands if you put thi
 else
   # Connect to rabbit mq server
   begin
-    RABBIT_MQ = Bunny.new "amqp://#{server[:user]}:#{server[:pass]}@#{server[:host]}:#{server[:port]}/#{server[:vhost]}", read_timeout: 30, heartbeat: 2
+    RABBIT_MQ = Bunny.new server_config
     RABBIT_MQ.start
 
     # Setup exchange and channel
     INDEXER_CHANNEL = RABBIT_MQ.create_channel
     INDEXER_EXCHANGE = INDEXER_CHANNEL.topic 'echo'
   rescue
-    Rails.logger.error "Cannot connect to Rabbit MQ"
+    Rails.logger.error 'Cannot connect to Rabbit MQ'
   end
 end
---
+++ readme.md
@@ -0,0 +1,5 @@
+stub
+====
+
+Event Ticket Management System
+
